8aba2d58fef36593688999dc457c4909821400eb
Tristan Chao <chaot372@gmail.com>
2024-08-02 18:47:45 +0000

    bryant is cooked

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 var $newEventBtn = document.querySelector('#new-event-btn');
 var $newEventDialog = document.querySelector('#new-event-dialog');
-if (!$newEventBtn) throw new Error('$newEventBtn query failed');
-if (!$newEventDialog) throw new Error('$newEventDialog query failed');
+if (!$newEventBtn)
+    throw new Error('$newEventBtn query failed');
+if (!$newEventDialog)
+    throw new Error('$newEventDialog query failed');
 $newEventBtn.addEventListener('click', function () {
-  $newEventDialog.showModal();
+    $newEventDialog.showModal();
 });
+var data = readData();
+function writeData() {
+    var dataJson = JSON.stringify(data);
+    localStorage.setItem('eventsData', dataJson);
+}
+function readData() {
+    var newDataJson = localStorage.getItem('eventsData');
+    var newData = JSON.parse(newDataJson);
+    return newData;
+}
--- main.ts
+++ main.ts
@@ -11,3 +11,28 @@ if (!$newEventDialog) throw new Error('$newEventDialog query failed');
 $newEventBtn.addEventListener('click', () => {
   $newEventDialog.showModal();
 });
+
+interface Event {
+  day: string;
+  time: string;
+  am: boolean;
+  details: string;
+}
+
+interface EventData {
+  editing: null | Event;
+  events: Event[];
+}
+
+const data: EventData = readData();
+
+function writeData(): void {
+  const dataJson = JSON.stringify(data);
+  localStorage.setItem('eventsData', dataJson);
+}
+
+function readData(): EventData {
+  const newDataJson = localStorage.getItem('eventsData');
+  const newData = JSON.parse(newDataJson);
+  return newData;
+}
